a5e24eb0403db2e5ab6f24a7584ba768146ac4a2
Kirigakureno <78303776+Kirigakureno@users.noreply.github.com>
2021-02-08 16:13:01 +0300

    \

---
+++ trycatchtask/taskabout try and catch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Tasktrycatch
+{
+    public static void Main()
+    {
+        float result;
+        float num;
+
+        Console.Write("Enter Number ");
+        try
+        {
+            num = Convert.ToSingle(Console.ReadLine());
+
+            result = (float)Math.Sqrt(num);
+            Console.WriteLine("The result is: {0}", result);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Error, I cant calculate this stuff");
+        }
+    }
+}
